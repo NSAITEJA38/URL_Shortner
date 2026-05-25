ebe97314d2013b137459c267bd217f56266a8269
cyberbuddy-0 <168121554+cyberbuddy-0@users.noreply.github.com>
2026-05-25 11:16:14 +0530

    modified Api's

--- Frontend/src/api/userApi.js
+++ Frontend/src/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"
+  baseURL: import.meta.env.VITE_API_BASE_URL || (import.meta.env.DEV ? "http://localhost:5000" : "https://url-shortner-1-yjbd.onrender.com")
 });
 
 export const registerUser = (data) => API.post("/user/register", data);
